e77b9341a9377a969c6fd8d1c153baab2cc1abd6
Sepand <12715168+SepandJP@users.noreply.github.com>
2023-04-26 11:20:09 +0330

    Use webpack

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # todo-js
 A simple todo list project with vanilla js
+
+This project has been created using **webpack-cli**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
---
+++ public/scripts/main.js
@@ -0,0 +1,43 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/b.js":
+/*!**************************!*\
+  !*** ./src/scripts/b.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("console.log('b');\n\n//# sourceURL=webpack://todo-js/./src/scripts/b.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/h.js":
+/*!**************************!*\
+  !*** ./src/scripts/h.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("console.log('h');\n\n//# sourceURL=webpack://todo-js/./src/scripts/h.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/scripts/b.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scripts/h.js"]();
+/******/ 	
+/******/ })()
+;
