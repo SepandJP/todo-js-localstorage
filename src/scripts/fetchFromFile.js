a47e6506bd79d2f623352248baf0bd457983b2ce
Sepand <12715168+SepandJP@users.noreply.github.com>
2023-04-30 18:28:57 +0330

    Fetch tasks data from file

--- src/scripts/fetchFromFile.js
+++ src/scripts/fetchFromFile.js
@@ -2,6 +2,17 @@
  * Recive data from file
  */
 
+const tasks_database_file_path = 'sample-tasks.json'
+console.log(tasks_database_file_path)
+
 export class fetchFromFile{
-    
+    async getTasks() {
+        try {
+            const result = await fetch(tasks_database_file_path);
+            const data = await result.json()
+            let tasks = data.items
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
